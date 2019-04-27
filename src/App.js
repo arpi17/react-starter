e11f3f47e7ebff85fd454751a1f2359669ab489e
Arpad Illyes <arpadillyes93@gmail.com>
2019-04-27 18:19:50 +0200

    Set up basic structure with hot reloading

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { hot } from 'react-hot-loader/root';
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello React World!</h1>
+      </div>
+    );
+  }
+}
+
+export default hot(App);
